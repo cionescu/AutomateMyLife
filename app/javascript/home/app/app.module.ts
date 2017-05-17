38acd11518c62588d0edd36699c8223b80e1dd1f
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-05-17 17:02:57 +0100

    feat(routing): Add the routing config

--- app/javascript/home/app/app.module.ts
+++ app/javascript/home/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RoutingModule } from './routing.module';
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/javascript/home/app/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard',
+  template: `
+    Home Page
+  `
+})
+export class DashboardComponent {
+  constructor() {
+  }
+}
